fa6f991bfa5bc7bfbadc9114ec63472ce7e1c941
eTarget1 <louisclifford10@yahoo.fr>
2019-09-03 15:54:20 -0400

    used MultiTap used Detox.

--- e2e/App.spec.js
+++ e2e/App.spec.js
@@ -11,19 +11,24 @@ describe('Example', () => {
     done();
   });
 
-  it('Should have a startButton', async(done) =>{
-    await expect(element(by.id('runningState'))).toHaveText('Stopped');
+  it('Should count to 1 if pressed once', async(done) =>{
+    await expect(element(by.id('runningState'))).toHaveText('0');
     await element(by.id('startButton')).tap();
-    await expect(element(by.id('runningState'))).toHaveText('Running');
+    await expect(element(by.id('runningState'))).toHaveText('1');
     done();
   });
 
-  it.only('Should write into TextInput', async(done) =>{
+  it('Should count to 4 if pressed 4 times', async(done) =>{
+    await expect(element(by.id('runningState'))).toHaveText('0');
+    await element(by.id('startButton')).multiTap(4);
+    await expect(element(by.id('runningState'))).toHaveText('4');
+    done();
+  });
+
+  it('Should write into TextInput', async(done) =>{
     await element(by.id('input')).typeText('Hello World!');
     await element(by.id('input')).clearText();
-
     await element(by.id('input')).typeText('Bye!');
-
   });
 
   // it('should show hello screen after tap', async (done) => {
